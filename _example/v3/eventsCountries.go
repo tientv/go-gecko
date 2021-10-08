cda433ac237072c792d30b864808ec115e684900
tienable <tranvinhtienit@gmail.com>
2021-10-08 16:33:09 +0700

    fix

--- _example/v3/eventsCountries.go
+++ _example/v3/eventsCountries.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	gecko "github.com/superoo7/go-gecko/v3"
+	gecko "github.com/tientv/go-gecko/v3"
 )
 
 func main() {
--- _example/v3/global.go
+++ _example/v3/global.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	gecko "github.com/superoo7/go-gecko/v3"
+	gecko "github.com/tientv/go-gecko/v3"
 )
 
 func main() {
